f67902b1cf7296fcfdad423f78a3b22c04b74b82
Andy Truong <dat13at1@hacke-2.student.lth.se>
2017-03-20 12:32:31 +0100

    init gui

---
+++ KrustyKookies/src/krustykookies/KookiesGUI.java
@@ -0,0 +1,9 @@
+package krustykookies;
+
+public class KookiesGUI {
+	private Database db;
+	
+	public KookiesGUI(Database db) {
+		this.db = db;
+	}
+}
---
+++ KrustyKookies/src/krustykookies/Krustykookies.java
@@ -0,0 +1,8 @@
+package krustykookies;
+
+public class Krustykookies {
+    public static void main(String[] args) {
+        Database db = new Database();
+        new KookiesGUI(db);
+    }
+}
