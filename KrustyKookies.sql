94be234bdf757be4fc07dfa0eca2035dd179a594
AleSim94 <aleksandar.simeunovic94@gmail.com>
2017-03-20 19:02:32 +0100

    La in värden i tabellen

--- KrustyKookies.sql
+++ KrustyKookies.sql
@@ -76,68 +76,61 @@ CREATE TABLE Shipments (
 );
 
 -- Insert data into the tables.
-INSERT INTO users (username, name, address, phone_number) VALUES
-('andy', 'AndyTruong', 'LTH', 0789456321),
-('andy2', 'Andy2', NULL, 0123456789),
-('andy3123', 'Andy3', 'dawdaw', 0258695847);
+INSERT INTO Customers (customer_name,address) VALUES
+('Finkakor AB','Helsingborg'),
+('Småbröd AB','Malmö'),
+('Kaffebröd AB','Landskorna'),
+('Bjudkakor AB','Ystad'),
+('Klaskakor AB','Trelleborg'),
+('Partykakor AB','Kristianstad'),
+('Gästkakor AB','Hässleholm'),
+('Skånekakor AB','Peterstorp'),
 
-INSERT INTO theaters (name, seats) VALUES
-('Royal', 498),
-('Filmstaden Entré', 60),
-('Filmstaden Storgatan', 63);
+
+INSERT INTO Ingredients (material_name,recipe_name,delivery_amount) VALUES
+('Nut ring','Flour',450),
+('Nut ring','Butter',450),
+('Nut ring','Icing sugar',190),
+('Nut ring','Roasted,chopped nuts',225),
+('Nut cookie','Fined-ground nuts',750),
+('Nut cookie','Ground, roasted nuts',625),
+('Nut cookie','Bread crumbs',125),
+('Nut cookie','Sugar',375),
+('Nut cookie','Egg whites',3.5),
+('Nut cookie','Chocolate',50),
+('Amneris','Marzipan',750),
+('Amneris','Butter',250),
+('Amneris','Eggs',250),
+('Amneris','Potato starch',25),
+('Amneris','Wheat flour',25),
+('Tango','Butter',200),
+('Tango','Sugar',250),
+('Tango','Flour',300),
+('Tango','Sodium bicarbonate',4),
+('Tango','Vanilla',2),
+('Almond delight','Butter',400),
+('Almond delight','Sugar',270),
+('Almond delight','Chopped almonds',279),
+('Almond delight','Flour',400),
+('Almond delight','Cinnamon',10),
+('Berliner','Flour',350),
+('Berliner','Butter',250),
+('Berliner','Icing sugar',100),
+('Berliner','Eggs',50),
+('Berliner','Vanilla sugar',5),
+('Berliner','Chocolate',50),
+
+
+
+
+
+
+
 
 INSERT INTO movies (name) VALUES
-('Alfons leker Einstein'),
-('Allied'),
-('American Pastoral'),
-('Arrival'),
-('Assassin''s Creed'),
-('Bamse och häxans dotter'),
-('Cosi fan tutte - opera från Parisoperan'),
-('Elle'),
-('En alldeles särskild dag - Klassiker'),
-('En man som heter Ove'),
-('En midommarnattsdröm - balett från Parisoperan'),
-('Fifty shades darker'),
-('Filmen om Badrock'),
-('Fyren mellan haven'),
-('Hundraettåringen som smet från notan och försvann'),
-('Jackie'),
-('Jag, Daniel Blake'),
-('Jätten'),
-('La La Land'),
-('Lion'),
-('Live by night'),
-('Manchester by the sea'),
-('Marcus & Martinus - Tillsammans mot drömmen'),
-('Min pappa Tonni Erdmann'),
-('Måste Gitt'),
-('Passengers'),
-('Pettson & Findus - Juligheter'),
-('Resident Evil - The Final Chapter'),
-('Rings'),
-('Rogue One: A Star Wars Story'),
-('Sing'),
-('Skönheten i allt'),
-('Split'),
-('The Bye Bye Man'),
-('The Handmaiden'),
-('The Lego Batman Movie'),
-('Trolls'),
-('Trubaduren - opera från Royal Opera House'),
-('Törnrosa - balett från Royal Opera House'),
-('Vaiana'),
-('Vem lurar Alfons'),
-('xXx: The Return of Xander Cage');
 
-INSERT INTO performances (name, date, theater_name, available_seats) VALUES
-('La La Land', '2017-02-01', 'Royal', 496),
-('La La Land', '2017-02-02', 'Filmstaden Storgatan', 63),
-('Jätten', '2017-02-02', 'Filmstaden Storgatan', 63);
 
-INSERT INTO reservations (username, movie_name, date) VALUES
-('andy', 'La La Land', '2017-02-01'),
-('andy2', 'La La Land', '2017-02-01');
+
 
 -- And re-enable foreign key checks.
 
