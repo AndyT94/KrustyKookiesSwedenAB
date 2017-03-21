ee42f4b552fba29ec5215b261c37e02bf5c4092f
AndyT94 <andytruong94@gmail.com>
2017-03-21 19:39:36 +0100

    rm comp

--- KrustyKookies/src/model/RawMaterial.java
+++ KrustyKookies/src/model/RawMaterial.java
@@ -3,7 +3,7 @@ package model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class RawMaterial implements Comparable<RawMaterial> {
+public class RawMaterial {
 	public String name;
 	public double amount;
 	public String unit;
@@ -12,10 +12,5 @@ public class RawMaterial implements Comparable<RawMaterial> {
 		this.name = rs.getString("material_name");
 		this.amount = rs.getFloat("material_amount");
 		this.unit = rs.getString("unit");
-	}
-
-	@Override
-	public int compareTo(RawMaterial other) {
-		return name.compareTo(other.name);
 	}
 }
