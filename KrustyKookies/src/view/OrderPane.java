d029c72ddfc3c4ab30855eddc5b69a1fe4c3d8f4
Andy Truong <dat13at1@jupiter-4.student.lth.se>
2017-03-22 13:22:34 +0100

    add order

--- KrustyKookies/src/view/OrderPane.java
+++ KrustyKookies/src/view/OrderPane.java
@@ -4,7 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -16,7 +20,6 @@ import javax.swing.JTextField;
 
 import model.Database;
 import model.Order;
-import view.DeliveryPane.ActionHandler;
 
 public class OrderPane extends BasicPane {
 	private static final long serialVersionUID = 1L;
@@ -26,16 +29,16 @@ public class OrderPane extends BasicPane {
 	private static final int CUSTOMER = 0;
 	private static final int ORDER = 1;
 	private static final int DATE = 2;
-	
+
 	private JTextField[] bottomTextFields;
 	private static final int BOTTOM_NBR_FIELDS = 2;
 	private static final int FROM = 0;
 	private static final int TO = 1;
-	
+
 	public OrderPane(Database db) {
 		super(db);
 	}
-	
+
 	public JComponent createMiddlePanel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
@@ -45,7 +48,7 @@ public class OrderPane extends BasicPane {
 		panel.add(scroll);
 		return panel;
 	}
-	
+
 	public JComponent createTopPanel() {
 		topTextFields = new JTextField[TOP_NBR_FIELDS];
 		JPanel panel = new JPanel();
@@ -78,11 +81,11 @@ public class OrderPane extends BasicPane {
 		panel.add(button);
 		return panel;
 	}
-	
+
 	public JComponent createBottomPanel() {
 		bottomTextFields = new JTextField[BOTTOM_NBR_FIELDS];
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(BOTTOM_NBR_FIELDS + 1, 1));
+		panel.setLayout(new GridLayout(BOTTOM_NBR_FIELDS + 2, 1));
 
 		JPanel from = new JPanel(new GridLayout(1, 2));
 		JLabel fromLabel = new JLabel("Date from ");
@@ -102,28 +105,92 @@ public class OrderPane extends BasicPane {
 		SearchHandler actHand = new SearchHandler();
 		button.addActionListener(actHand);
 		panel.add(button);
+
+		panel.add(messageLabel);
 		return panel;
 	}
-	
+
 	public void entryActions() {
 		text.setText("");
 		List<Order> orders = db.getOrders();
-		text.append(String.format("%-1s\t %-15s\t %-1s\t %-10s\t %4s\n", "Order ID", "Recipe", "Amount", "Customer", "Deliver By"));
+		text.append(String.format("%-1s\t %-15s\t %-1s\t %-10s\t %4s\n", "Order ID", "Recipe", "Amount", "Customer",
+				"Deliver By"));
 		text.append("\n");
 		for (Order o : orders) {
-			text.append(String.format("%-1s\t %-15s\t %-1s\t %-10s\t %4s\n", o.id, o.recipe, o.amount, o.customer, o.date));
+			text.append(
+					String.format("%-1s\t %-15s\t %-1s\t %-10s\t %4s\n", o.id, o.recipe, o.amount, o.customer, o.date));
 		}
 	}
-	
+
 	class OrderHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			
+			String customer = topTextFields[CUSTOMER].getText();
+			String order = topTextFields[ORDER].getText();
+			String date = topTextFields[DATE].getText();
+
+			String msg = "";
+			Map<String, String> cookies = null;
+			if (date.isEmpty() || customer.isEmpty() || order.isEmpty()) {
+				msg += "Please fill in all fields! ";
+			} else {
+				if (!isDate(date)) {
+					msg += "Invalid date (Format: YYYY-MM-DD)! ";
+				}
+
+				if (!db.hasCustomer(customer)) {
+					msg += "No such customer! ";
+				}
+
+				String[] amount = order.split(",");
+				cookies = new HashMap<String, String>();
+				for (int i = 0; i < amount.length; i++) {
+					String[] data = amount[i].trim().split("\\s");
+					String nbr = data[0];
+					String recipe = "";
+					for (int j = 1; j < data.length; j++) {
+						recipe += data[j] + " ";
+					}
+					cookies.put(recipe.trim(), nbr);
+				}
+				for(String key : cookies.keySet()) {
+					if(!db.hasRecipe(key)) {
+						msg += "No such recipe! ";
+						break;
+					}
+				}
+			}
+
+			if (msg.isEmpty()) {
+				db.addOrder(customer, cookies, date);
+				entryActions();
+				displayMessage("");
+			} else {
+				displayMessage(msg);
+			}
+			clearFields();
+		}
+
+		private void clearFields() {
+			for (int i = 0; i < topTextFields.length; i++) {
+				topTextFields[i].setText("");
+			}
 		}
 	}
-	
+
+	private boolean isDate(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		format.setLenient(false);
+		try {
+			format.parse(date);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
+	}
+
 	class SearchHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			
+
 		}
 	}
 }
